d0a59ddd32670accec4af154bd87159aac981359
Carlos Renato <carlos_sdossantos@hotmail.com>
2026-04-11 15:31:36 -0300

    feat: inicializando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className="text-red-500 text-5xl">Hello World</p>
+    </>
+  )
+}
+
+export default App
