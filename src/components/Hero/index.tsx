8acc469cab2b81ff380fc5ac4c4e8f170798f4d6
Carlos Renato <carlos_sdossantos@hotmail.com>
2026-04-21 13:15:59 -0300

    feat: adicionada rota de categoria

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,24 +1,32 @@
 import Banner from "@/assets/images/banner.jpg";
 import { Button } from "../Button";
 import { Overlay } from "../Overlay";
+import { useRouter } from "@tanstack/react-router";
 
 export const Hero = () => {
+  const router = useRouter();
+
   return (
     <div className="container">
       <section className="relative h-125 rounded-[20px] mb-10">
         <img
           src={Banner}
-          alt="Homem sentado com tênis da SyntaxWear"
+          alt="Homem sentado com os tênis da SyntaxWear"
           className="w-full h-full object-cover rounded-[20px]"
         />
 
         <Overlay
-          title="Krypton One"
+          title="Kripton One"
           subtitle="Transforme qualquer passo em presença"
           className="bottom-0 px-6 md:px-24 pb-24 justify-end md:items-end"
         >
-          <Button variant="secondary">Ver modelos</Button>
-          <Button size="lg">Comprar</Button>
+          <Button
+            variant="secondary"
+            onClick={() => router.navigate({ to: "/products" })}
+          >
+            Ver modelos
+          </Button>
+          <Button>Comprar</Button>
         </Overlay>
       </section>
     </div>
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,15 +1,15 @@
-import type { Product } from "../interfaces/product";
-import { ProductCard } from "../ProductCard";
+import type { Product } from "../../interfaces/product"
+import { ProductCard } from "../ProductCard"
 
 interface ProductListProps {
-    products: Product[];
+    products: Product[]
 }
 
 export const ProductList = ({ products } : ProductListProps) => {
     return (
-        <section className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(350px,1fr))] mb-10">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
+        <section className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))] xl:grid-cols-3 mb-10">
+            {products.map(product => (
+                <ProductCard key={product.id} product={product}/>
             ))}
         </section>
     )
