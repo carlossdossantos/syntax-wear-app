0aa819204c6ea9b9fc18ea26a64bd1e6a8ca8440
Carlos Renato <carlos_sdossantos@hotmail.com>
2026-05-05 15:43:34 -0300

    feat: adicionada integração da tela de produtos  filtrados por categoria e paginação

---
+++ src/services/categoryService.ts
@@ -0,0 +1,23 @@
+import { notFound } from "@tanstack/react-router";
+
+export async function getCategoryByName(name: string){
+    const params = new URLSearchParams({
+        page: '1',
+        limit: '1',
+        search: name
+    })
+
+    const response = await fetch(`http://localhost:3000/categories?${params.toString()}`);
+
+    if(!response.ok){
+        throw notFound();
+    }
+
+    const result = await response.json();
+
+    if(!result.data || result.data.length === 0){
+        throw notFound();
+    }
+
+    return result.data[0];
+}
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -42,3 +42,20 @@ export async function getProducts({page, limit = DEFAULT_LIMIT} : GetProductPara
   }
 
 }
+
+export async function getProductByCategoryId(categoryId: number, paginationParams?: GetProductParams ) : Promise<ProductResponse> {
+    const params = new URLSearchParams({
+      page: paginationParams?.page.toString() || '1', 
+      limit: (paginationParams?.limit || DEFAULT_LIMIT).toString(),
+      categoryId: categoryId.toString()
+    });
+
+    const response = await fetch(`${API_BASE_URL}/products?${params.toString()}`);
+
+    if(!response.ok){
+      throw new Error(`Erro ao buscar produtos por categoria: ${response.statusText}`);
+    }
+
+    return await response.json();
+    
+}
